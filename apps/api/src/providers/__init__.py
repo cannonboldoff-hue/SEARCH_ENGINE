77895cf71f80fa7923f77ffe162f1e361d0cceb8
cannonboldoff-hue <cannonboldoff-hue@users.noreply.github.com>
2026-02-05 19:39:30 +0530

    fixed some issues

--- apps/api/src/providers/__init__.py
+++ apps/api/src/providers/__init__.py
@@ -1,8 +1,9 @@
-from .chat import ChatServiceError, get_chat_provider
+from .chat import ChatServiceError, ChatRateLimitError, get_chat_provider
 from .embedding import EmbeddingServiceError, get_embedding_provider
 
 __all__ = [
     "ChatServiceError",
+    "ChatRateLimitError",
     "get_chat_provider",
     "EmbeddingServiceError",
     "get_embedding_provider",
